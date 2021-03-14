f2c339a1650d968f3673b553659dfee42d343c2e
HasibulSarker <63333502+HasibulSarker@users.noreply.github.com>
2021-03-14 09:55:46 +0600

    fixes

---
+++ HADOM/script/script.js
@@ -0,0 +1,41 @@
+//  Start Of JavaScript//
+
+document.querySelector('body').addEventListener("dblclick", function () {
+
+    var head = document.querySelector("head").innerHTML;
+
+    var newhead = '<link rel="stylesheet" href="css/style-old.css">';
+
+    head+=newhead;
+
+    document.querySelector("head").innerHTML = head;
+})
+
+const a = "Data/about.txt";
+
+const b ="Posts/post.txt";
+
+const c ="Posts/post2.txt";
+
+function getdata(link) {
+
+    const b = link;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", b);
+
+    xhr.send();
+
+    xhr.onload = function () {
+
+        const c = xhr.responseText;
+        
+        const d = document.getElementById("container");
+        d.innerHTML = c;
+        
+    }
+    
+}
+
+// End of Javascript //
